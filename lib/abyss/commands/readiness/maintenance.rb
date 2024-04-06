a7adbfde797e93973c9abfc1e61b1fff12dc2bdd
Aotokitsuruya <contact@aotoki.me>
2024-04-06 22:14:35 +0800

    Add delay option to readiness maintenance command

--- lib/abyss/commands/readiness/maintenance.rb
+++ lib/abyss/commands/readiness/maintenance.rb
@@ -9,9 +9,13 @@ module Abyss
       class Maintenance < Dry::CLI::Command
         desc 'Check the maintenance mode is disabled'
 
+        option :delay, type: :integer, default: 0, desc: 'The delay in seconds before checking the readiness'
         option :wait, type: :boolean, default: false, desc: 'Wait until the maintenance mode is disabled'
 
         def call(**options)
+          delay = options.fetch(:delay, 0).to_i
+          sleep delay if delay.positive?
+
           while Abyss::Maintenance.enabled?
             puts 'The server is in maintenance mode'
             exit 1 unless options[:wait]
