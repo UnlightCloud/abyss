5b88ec6b6ac022be0e8437b09b14d28d8d787645
Aotokitsuruya <contact@aotoki.me>
2024-04-10 18:08:18 +0800

    Add container prepare action

--- lib/abyss/application.rb
+++ lib/abyss/application.rb
@@ -118,17 +118,63 @@ module Abyss
       # @return [self]
       #
       # @since 0.1.0
-      def prepare
+      def prepare(provider_name = nil)
+        if provider_name
+          container.prepare(provider_name)
+        else
+          prepare_app
+        end
+
+        self
+      end
+
+      private
+
+      # @api private
+      # @since 0.1.0
+      def prepare_app
         return self if prepared?
 
         config.finalize!
 
-        namespace.const_set(:Container, container)
-        namespace.const_set(:Deps, container.injector)
+        prepare_container_constants
+        prepare_container_plugins
+        prepare_container_base_config
 
         @prepared = true
 
         self
+      end
+
+      # @api private
+      # @since 0.1.0
+      def prepare_container_constants
+        namespace.const_set(:Container, container)
+        namespace.const_set(:Deps, container.injector)
+      end
+
+      # @api private
+      # @since 0.1.0
+      def prepare_container_plugins
+        container.use(:env, inferrer: -> { Abyss.env })
+        container.use(
+          :zeitwerk,
+          loader: autoloader,
+          run_setup: false,
+          eager_load: false
+        )
+      end
+
+      # @api private
+      # @since 0.1.0
+      def prepare_container_base_config
+        container.config.name = app_name.to_sym
+        container.config.root = Abyss.root
+        container.config.provider_dirs = [File.join('config', 'providers')]
+        container.config.registrations_dir = File.join('config', 'registrations')
+
+        container.config.env = config.env
+        container.config.inflector = config.inflector
       end
     end
   end
